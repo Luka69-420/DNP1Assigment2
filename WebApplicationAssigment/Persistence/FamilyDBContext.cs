c72a16a7c2a0ca0a97a8e7a2544580a89b094345
Luka Udovicic <lukaudovicic10@gmail.com>
2021-11-23 17:23:24 +0100

    Database stuff

---
+++ WebApplicationAssigment/Persistence/FamilyDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace WebApplicationAssigment.Persistence
+{
+    public class FamilyDbContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<Adult> Adults { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source = Family.db");
+        }
+      
+    }
+}
