1b6351e779fb0a2fcfcbf4ccafd6aebf905e1925
Luka Udovicic <lukaudovicic10@gmail.com>
2021-11-28 22:30:06 +0100

    Database stuff

---
+++ WebApplicationAssigment/Migrations/20211128203347_Family.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationAssigment.Migrations
+{
+    public partial class Family : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Families",
+                columns: table => new
+                {
+                    StreetName = table.Column<string>(type: "TEXT", nullable: false),
+                    HouseNumber = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Families", x => x.StreetName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interests",
+                columns: table => new
+                {
+                    InterestId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interests", x => x.InterestId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    JobId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Salary = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.JobId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    HairColor = table.Column<string>(type: "TEXT", nullable: true),
+                    EyeColor = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Weight = table.Column<float>(type: "REAL", nullable: false),
+                    Height = table.Column<int>(type: "INTEGER", nullable: false),
+                    Sex = table.Column<string>(type: "TEXT", nullable: true),
+                    AdultId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    JobId = table.Column<int>(type: "INTEGER", nullable: true),
+                    FamilyStreetName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adults_Families_FamilyStreetName",
+                        column: x => x.FamilyStreetName,
+                        principalTable: "Families",
+                        principalColumn: "StreetName",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adults_Jobs_JobId",
+                        column: x => x.JobId,
+                        principalTable: "Jobs",
+                        principalColumn: "JobId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Adults_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Children",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChildId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FamilyStreetName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Children", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Children_Families_FamilyStreetName",
+                        column: x => x.FamilyStreetName,
+                        principalTable: "Families",
+                        principalColumn: "StreetName",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Children_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChildInterest",
+                columns: table => new
+                {
+                    ChildrenId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InterestsInterestId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChildInterest", x => new { x.ChildrenId, x.InterestsInterestId });
+                    table.ForeignKey(
+                        name: "FK_ChildInterest_Children_ChildrenId",
+                        column: x => x.ChildrenId,
+                        principalTable: "Children",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChildInterest_Interests_InterestsInterestId",
+                        column: x => x.InterestsInterestId,
+                        principalTable: "Interests",
+                        principalColumn: "InterestId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    PetId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Species = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChildId = table.Column<int>(type: "INTEGER", nullable: true),
+                    FamilyStreetName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.PetId);
+                    table.ForeignKey(
+                        name: "FK_Pets_Children_ChildId",
+                        column: x => x.ChildId,
+                        principalTable: "Children",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pets_Families_FamilyStreetName",
+                        column: x => x.FamilyStreetName,
+                        principalTable: "Families",
+                        principalColumn: "StreetName",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adults_FamilyStreetName",
+                table: "Adults",
+                column: "FamilyStreetName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adults_JobId",
+                table: "Adults",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChildInterest_InterestsInterestId",
+                table: "ChildInterest",
+                column: "InterestsInterestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Children_FamilyStreetName",
+                table: "Children",
+                column: "FamilyStreetName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_ChildId",
+                table: "Pets",
+                column: "ChildId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_FamilyStreetName",
+                table: "Pets",
+                column: "FamilyStreetName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adults");
+
+            migrationBuilder.DropTable(
+                name: "ChildInterest");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Jobs");
+
+            migrationBuilder.DropTable(
+                name: "Interests");
+
+            migrationBuilder.DropTable(
+                name: "Children");
+
+            migrationBuilder.DropTable(
+                name: "Families");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ WebApplicationAssigment/Persistence/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Models;
+
+namespace WebApplicationAssigment.Persistence
+{
+    public static class DbInitializer
+    {
+        
+        public static void Initialize(FamilyDbContext context)
+        {
+            if (context.Adults.Any())
+            {
+                return;
+            }
+
+            var user = new User()
+            {
+                Username = "username",
+                Password = "password"
+            };
+            context.Users.Add(user);
+        }
+    }
+}
